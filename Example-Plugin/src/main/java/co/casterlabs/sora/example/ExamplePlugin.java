81087cb26b3ebb1399fc11a5aba4d8114e1ed5a4
Ender <33337309+e3ndr@users.noreply.github.com>
2021-03-06 13:18:01 -0600

    Update the example plugin for the v2 api.

--- Example-Plugin/src/main/java/co/casterlabs/sora/example/ExamplePlugin.java
+++ Example-Plugin/src/main/java/co/casterlabs/sora/example/ExamplePlugin.java
@@ -1,17 +1,17 @@
 package co.casterlabs.sora.example;
 
+import co.casterlabs.rakurai.io.http.HttpResponse;
+import co.casterlabs.rakurai.io.http.HttpSession;
+import co.casterlabs.rakurai.io.http.StandardHttpStatus;
+import co.casterlabs.rakurai.io.http.websocket.Websocket;
+import co.casterlabs.rakurai.io.http.websocket.WebsocketListener;
+import co.casterlabs.rakurai.io.http.websocket.WebsocketSession;
 import co.casterlabs.sora.Sora;
 import co.casterlabs.sora.api.PluginImplementation;
 import co.casterlabs.sora.api.SoraPlugin;
 import co.casterlabs.sora.api.http.HttpProvider;
-import co.casterlabs.sora.api.http.HttpResponse;
-import co.casterlabs.sora.api.http.HttpSession;
-import co.casterlabs.sora.api.http.HttpStatus;
 import co.casterlabs.sora.api.http.annotations.HttpEndpoint;
-import co.casterlabs.sora.api.websockets.Websocket;
-import co.casterlabs.sora.api.websockets.WebsocketListener;
 import co.casterlabs.sora.api.websockets.WebsocketProvider;
-import co.casterlabs.sora.api.websockets.WebsocketSession;
 import co.casterlabs.sora.api.websockets.annotations.WebsocketEndpoint;
 import lombok.SneakyThrows;
 
@@ -50,7 +50,7 @@ public class ExamplePlugin extends SoraPlugin implements HttpProvider, Websocket
 
     @HttpEndpoint(uri = "/example")
     public HttpResponse onExample(HttpSession session) {
-        return HttpResponse.newFixedLengthResponse(HttpStatus.OK, "Hello " + session.getRemoteIpAddress() + "!");
+        return HttpResponse.newFixedLengthResponse(StandardHttpStatus.OK, "Hello " + session.getRemoteIpAddress() + "!");
     }
 
     @WebsocketEndpoint(uri = "/echo")
@@ -81,7 +81,7 @@ public class ExamplePlugin extends SoraPlugin implements HttpProvider, Websocket
 
     @Override
     public HttpResponse onNoProvider(HttpSession session) {
-        return HttpResponse.newFixedLengthResponse(HttpStatus.NOT_FOUND, "Navigate to /example or open a Websocket on /echo");
+        return HttpResponse.newFixedLengthResponse(StandardHttpStatus.NOT_FOUND, "Navigate to /example or open a Websocket on /echo");
     }
 
 }
