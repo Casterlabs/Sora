9ed00b42e5b4f821348a082856787d4fd0f24e49
Ender <33337309+e3ndr@users.noreply.github.com>
2022-01-16 08:22:57 -0600

    Cleanup a Javadoc.

--- Sora-Api/src/main/java/co/casterlabs/sora/api/http/HttpPreProcessor.java
+++ Sora-Api/src/main/java/co/casterlabs/sora/api/http/HttpPreProcessor.java
@@ -8,7 +8,7 @@ import lombok.NonNull;
 public interface HttpPreProcessor<T> {
 
     /**
-     * @return A response to intercept the connection.
+     * @return (Optionally) A response to intercept the connection.
      */
     public @Nullable HttpResponse preprocessHttpSession(@NonNull T data, @NonNull SoraHttpSession session);
 
