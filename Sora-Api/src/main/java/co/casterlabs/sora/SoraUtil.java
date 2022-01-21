ce51b30066c81f716d7985d825f7d084dbfbd100
Ender <33337309+e3ndr@users.noreply.github.com>
2022-01-21 12:44:51 -0600

    Api: Remove some unused code.

--- Sora-Api/src/main/java/co/casterlabs/sora/SoraUtil.java
+++ Sora-Api/src/main/java/co/casterlabs/sora/SoraUtil.java
@@ -1,31 +1,6 @@
 package co.casterlabs.sora;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import lombok.NonNull;
-
 public class SoraUtil {
-    private static final int BUFFER_SIZE = 4096;
-
-    public static byte[] readAllBytes(@NonNull InputStream in, int length) throws IOException {
-        byte[] bytes = new byte[length];
-
-        in.read(bytes);
-        in.close();
-
-        return bytes;
-    }
-
-    public static void writeInputStreamToOutputStream(@NonNull InputStream in, @NonNull OutputStream out) throws IOException {
-        byte[] buffer = new byte[BUFFER_SIZE];
-        int len = 0;
-
-        while ((len = in.read(buffer)) != -1) {
-            out.write(buffer, 0, len);
-        }
-    }
 
     public static <T> boolean arrayContains(T[] arr, T find) {
         for (T item : arr) {
