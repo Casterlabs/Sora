3d23f5ee250f060644facd5fe8db5cd78c7e88c9
Ender <33337309+e3ndr@users.noreply.github.com>
2023-02-25 18:43:26 -0600

    Api: Add better registration methods.
    The old ones sucked.

--- Sora-Api/src/main/java/co/casterlabs/sora/Sora.java
+++ Sora-Api/src/main/java/co/casterlabs/sora/Sora.java
@@ -11,13 +11,31 @@ import lombok.NonNull;
 
 public interface Sora {
 
-    public void addHttpProvider(@NonNull SoraPlugin plugin, @NonNull HttpProvider httpProvider);
+    /**
+     * provider must extend {@link HttpProvider} or {@link WebsocketProvider} or
+     * both to be properly registered.
+     */
+    default void addProvider(@NonNull SoraPlugin plugin, @NonNull Object provider) {
+        if (provider instanceof HttpProvider) {
+            this.addHttpProvider(plugin, (HttpProvider) provider);
+        }
+        if (provider instanceof WebsocketProvider) {
+            this.addWebsocketProvider(plugin, (WebsocketProvider) provider);
+        }
+    }
 
-    public void addWebsocketProvider(@NonNull SoraPlugin plugin, @NonNull WebsocketProvider websocketProvider);
-
-    public void registerHttpPreProcessor(@NonNull SoraPlugin plugin, @NonNull String id, @NonNull HttpPreProcessor<?> httpPreProcessor);
-
-    public void registerWebsocketPreProcessor(@NonNull SoraPlugin plugin, @NonNull String id, @NonNull WebsocketPreProcessor<?> websocketPreProcessor);
+    /**
+     * preProcessor must extend {@link HttpPreProcessor} or
+     * {@link WebsocketPreProcessor} or both to be properly registered.
+     */
+    default void registerPreProcessor(@NonNull SoraPlugin plugin, @NonNull String id, @NonNull Object preProcessor) {
+        if (preProcessor instanceof HttpPreProcessor) {
+            this.registerHttpPreProcessor(plugin, id, (HttpPreProcessor<?>) preProcessor);
+        }
+        if (preProcessor instanceof WebsocketPreProcessor) {
+            this.registerWebsocketPreProcessor(plugin, id, (WebsocketPreProcessor<?>) preProcessor);
+        }
+    }
 
     public @Nullable SoraPlugin getPluginById(@NonNull String id);
 
@@ -27,5 +45,33 @@ public interface Sora {
      */
     @Deprecated
     public ClassLoader getGlobalClassLoader();
+
+    /* ---------------- */
+    /* Deprecated       */
+    /* ---------------- */
+
+    /**
+     * @deprecated Use {@link #addProvider(SoraPlugin, Object)}
+     */
+    @Deprecated
+    public void addHttpProvider(@NonNull SoraPlugin plugin, @NonNull HttpProvider httpProvider);
+
+    /**
+     * @deprecated Use {@link #addProvider(SoraPlugin, Object)}
+     */
+    @Deprecated
+    public void addWebsocketProvider(@NonNull SoraPlugin plugin, @NonNull WebsocketProvider websocketProvider);
+
+    /**
+     * @deprecated Use {@link #registerPreProcessor(SoraPlugin, String, Object)}
+     */
+    @Deprecated
+    public void registerHttpPreProcessor(@NonNull SoraPlugin plugin, @NonNull String id, @NonNull HttpPreProcessor<?> httpPreProcessor);
+
+    /**
+     * @deprecated Use {@link #registerPreProcessor(SoraPlugin, String, Object)}
+     */
+    @Deprecated
+    public void registerWebsocketPreProcessor(@NonNull SoraPlugin plugin, @NonNull String id, @NonNull WebsocketPreProcessor<?> websocketPreProcessor);
 
 }
